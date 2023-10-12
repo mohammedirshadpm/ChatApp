c4fe4c093289663526d1785e99f3346fd2a1a59c
IrshadFlutter <m38648747@gmail.com>
2023-10-12 08:21:52 +0530

    Screen217a

--- lib/Screen27.dart
+++ lib/Screen27.dart
@@ -58,11 +58,231 @@ class _Screen27State extends State<Screen27> {
                 )
             ),
           ),
-          Image.asset(
-            "assets/7.png",
-            width: 19,
-            height: 19,
+          SizedBox(
+            height: 29,
           ),
+          Padding(
+            padding: const EdgeInsets.only(left: 45),
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/7.png",
+                  width: 29,
+                  height: 29,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 52),
+                  child: Image.asset(
+                    "assets/8.png",
+                    width: 29,
+                    height: 29,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 53),
+                  child: Image.asset(
+                    "assets/9.png",
+                    width: 29,
+                    height: 29,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 53),
+                  child: Image.asset(
+                    "assets/10.png",
+                    width: 29,
+                    height: 29,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 34,
+          ),
+          Container(
+              width: 375,
+              height: 440,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft:  Radius.circular(0),
+                    bottomRight:  Radius.circular(0),
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+
+                  ),
+                  color: Colors.white),
+            child: SingleChildScrollView(
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 41,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Text(
+                        "Display Name",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff797C7B)
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28),
+                    child: Text(
+                        "Jhon Abraham",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Text(
+                        "Email Address",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28),
+                    child: Text(
+                        "jhonabraham20@gmail.com",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Text(
+                        "Address",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28),
+                    child: Text(
+                        "33 street west subidbazar,sylhet",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Text(
+                        "Phone Number",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28),
+                    child: Text(
+                        "(320) 555-0104",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Row(
+                      children: [
+                        Text(
+                            "Media Shared",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            )
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 184),
+                          child: Text(
+                              "View All",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xff20A090)
+                              )
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 28),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/11.png",
+                          width: 92,
+                          height: 92,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Image.asset(
+                            "assets/12.png",
+                            width: 92,
+                            height: 92,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Image.asset(
+                            "assets/13a.png",
+                            width: 92,
+                            height: 92,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
